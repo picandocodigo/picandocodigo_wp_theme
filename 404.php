da6385e1f53b8b0d3779bcec977ffa978c97400b
Fernando Briano <fernando@picandocodigo.net>
2013-01-27 15:27:08 -0200

    Error pages

--- 404.php
+++ 404.php
@@ -1,24 +1,23 @@
+<?php get_header(); ?>
 <?php
   $mensajes = array('Tengo otros mensajes de error mucho mas graciosos, informativos e interesantes que &eacute;ste.',
         'Uno de nuestros monos entrenados para programar en GNU Emacs se escap&oacute; y se llev&oacute; algunas p&aacute;ginas con &eacute;l...',
-        'Esto es vergonzoso... &#161;pero alguien tendr&aacute; que pagar!',
         'No entres en p&aacute;nico, &#191;trajiste tu toalla?',
         'No te sientas mal, estas cosas pasan. <br/>Debe ser culpa de Flash...',
         'Presione la tecla "Any" para continuar.',
         'Tengo un webmaster perezoso que todav&iacute;a no me ha terminado, y suele dejar enlaces rotos por ah&iacute;.',
-        'Presione la tecla "Any" para continuar.',
         'Presione la combinaci&oacute;n de teclas "Alt +F4" para realizar un test de inteligencia.',
-        'Esto es vergonzoso... pero alguien tendr&aacute; que pagar!',
         '&#191;Alguien ha visto mi engrampadora?',
         'Es la hora de manteca de man&iacute; con jalea',
         'All Your Base Are Belong To Us',
         'La p&aacute;gina ha sido atacada por Velociraptors... &#161;malditos gorriones superdesarrollados!',
-        'Encontraste el mensaje de error perdido. rep&oacute;rtalo en el <a href="http://foro.picandocodigo.net">foro</a> y reclama tu premio de dos &aacute;tomos de hidr&oacute;geno.',
+        'Encontraste el mensaje de error perdido. rep&oacute;rtalo y reclama tu premio de dos &aacute;tomos de hidr&oacute;geno.',
         'Este mensaje fue escrito a las 03:05 a.m. un s&aacute;bado a la noche.',
         'Valoramos mucho su tiempo, por favor aguarde en l&iacute;nea mientras uno de nuestros operadores se desocupa.',
         'Dividiendo entre cero, por favor aguarde en l&iacute;nea.',
-        'Te invito a acercarte al <strong><a href="http://foro.picandocodigo.net">foro</a></strong>, para unirte a la comunidad, hacer nuevos amigos, y contarnos del enlace roto.',
-        'Ayuda, me tienen atrapado en un s&oacute;tano escribiendo mensajes para este sitio.')
+        'Ayuda, me tienen atrapado en un s&oacute;tano escribiendo mensajes para este sitio.',
+        'Es culpa de PHP, estas cosas con Ruby no pasan.',
+        'El servidor Tomcat donde tenemos desplegada la aplicaci&oacute;n est&aacute; persiguiendo un puntero l&aacute;ser. Sospechamos que se cansar&aacute; en breve y volver&aacute; a sus tareas diarias como servidor')
 ?>
 
 <div class="twelve columns">
@@ -42,3 +41,5 @@
   </div>
 
 </div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
