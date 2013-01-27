88ef3116e310d4bb05adf981280005c93dd8eff7
Fernando Briano <fernando@picandocodigo.net>
2013-01-27 02:09:57 -0200

    Decent footer

--- footer.php
+++ footer.php
@@ -1,3 +1,37 @@
 <div id="footer" class="sixteen columns">
-  Testing footer
+  <div class="three columns footer_left">
+    <ul>
+      <li>
+        <a href="http://picandocodigo.net/about/">
+          Acerca de
+        </a>
+      </li>
+      <li>
+        <a href="http://picandocodigo.net/about/licencia/">
+          Licencia
+        </a>
+      </li>
+      <li>
+        <a href="http://picandocodigo.net/sitemap/">
+          Mapa del sitio
+        </a>
+      </li>
+      <li>
+        <a href="http://picandocodigo.net/contacto/">
+          Contacto
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="twelve columns footer_right">
+    <p class="cc">
+      Los contenidos de éste sitio se encuentran publicados bajo <a href="http://picandocodigo.net/about/licencia/">licencia Creative Commons</a> a menos que sea indicado de otra forma.<br/>
+
+      Todo el <strong>c&oacute;digo fuente</strong> publicado se libera bajo una licencia de software libre como <a href="http://picandocodigo.net/downloads/gpl.txt">GPL</a> o similar.
+    </p>
+
+    <p class="wordpress">
+      Picando Código está orgulloso de ser gestionado con <a href="http://wordpress.org">WordPress</a> y es alojado por <a href="http://picod.net/hg" title="Hostgator">Hostgator</a>.
+    </p>
+  </div>
 </div>
