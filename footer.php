bfd4f63c4212b2e3a025fddcfe7deea4c215e50f
Fernando Briano <fernando@picandocodigo.net>
2013-01-27 18:55:32 -0200

    Adds wp_footer for admin bar, Analytics code

--- footer.php
+++ footer.php
@@ -34,3 +34,12 @@
     </p>
   </div>
 </div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+	_uacct = "UA-2107177-1";
+	urchinTracker();
+</script>
+<?php wp_footer(); ?>
+</body>
+</html>
